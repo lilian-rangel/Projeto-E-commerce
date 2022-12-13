daa280f4e0fd060a5bdf2e060e2c1a71295810c5
lilian-rangel <lilianrm.nutri@gmail.com>
2022-12-13 15:44:20 -0300

    :construction: feat: add cart box

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,13 @@ let search = document.querySelector(".search-box");
 
 document.querySelector("#search-icon").onclick = () =>{
     search.classList.toggle("active");
+    cart.classList.remove("active");
+}
+
+let cart = document.querySelector(".cart");
+
+document.querySelector("#cart-icon").onclick = () =>{
+    cart.classList.toggle("active");
+    search.classList.remove("active");
+
 }
