d5722ad5421efaf903dfb3088cce17e2a1b2dc72
lilian-rangel <lilianrm.nutri@gmail.com>
2022-12-14 10:30:40 -0300

    :construction: feat: add user

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ let search = document.querySelector(".search-box");
 document.querySelector("#search-icon").onclick = () =>{
     search.classList.toggle("active");
     cart.classList.remove("active");
+    user.classList.remove("active");
 }
 
 let cart = document.querySelector(".cart");
@@ -10,5 +11,13 @@ let cart = document.querySelector(".cart");
 document.querySelector("#cart-icon").onclick = () =>{
     cart.classList.toggle("active");
     search.classList.remove("active");
+    user.classList.remove("active");
+}
 
+let user = document.querySelector(".user");
+
+document.querySelector("#user-icon").onclick = () =>{
+    user.classList.toggle("active");
+    cart.classList.remove("active");
+    search.classList.remove("active");
 }
