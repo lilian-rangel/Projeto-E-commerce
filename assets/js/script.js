d3eb82e1f0c35c6984488258df60e44502bbdc1c
lilian-rangel <lilianrm.nutri@gmail.com>
2022-12-16 12:35:15 -0300

    :sparkles: feat: add media query

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ document.querySelector("#search-icon").onclick = () =>{
     search.classList.toggle("active");
     cart.classList.remove("active");
     user.classList.remove("active");
+    navbar.classList.remove("active");
 }
 
 let cart = document.querySelector(".cart");
@@ -12,6 +13,7 @@ document.querySelector("#cart-icon").onclick = () =>{
     cart.classList.toggle("active");
     search.classList.remove("active");
     user.classList.remove("active");
+    navbar.classList.remove("active");
 }
 
 let user = document.querySelector(".user");
@@ -20,6 +22,24 @@ document.querySelector("#user-icon").onclick = () =>{
     user.classList.toggle("active");
     cart.classList.remove("active");
     search.classList.remove("active");
+    navbar.classList.remove("active");
+
+}
+
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-icon").onclick = () =>{
+    navbar.classList.toggle("active");
+    search.classList.remove("active");
+    cart.classList.remove("active");
+    user.classList.remove("active");
+}
+
+window.onscroll = () => {
+  search.classList.remove("active");
+  cart.classList.remove("active");
+  user.classList.remove("active");
+  navbar.classList.remove("active");
 }
 
 // Scroll navBar
